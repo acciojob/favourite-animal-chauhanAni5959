a5df1e8c8c1e0a55e374f21189f391af85151732
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2025-09-26 11:03:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,4 @@
-// Placeholder for JavaScript; can add interactivity here
-console.log("Page loaded, favourite animal displayed.");
+// On clicking the image, toggle a border
+document.getElementById('animalImage').addEventListener('click', function() {
+  this.style.border = this.style.border ? '' : '5px solid #1877f2';
+});
