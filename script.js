6cce6fb26582774925a8c3509f53efb7a9ff6d46
Ankit Singh <142376440+chauhanAni5959@users.noreply.github.com>
2025-09-26 11:16:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
-// On clicking the image, toggle a border
-document.getElementById('animalImage').addEventListener('click', function() {
-  this.style.border = this.style.border ? '' : '5px solid #1877f2';
+// Add click toggle border on the image for interaction
+document.getElementById('animalImage').addEventListener('click', function () {
+  if (this.style.border) {
+    this.style.border = '';
+  } else {
+    this.style.border = '5px solid #42b72a';
+  }
 });
